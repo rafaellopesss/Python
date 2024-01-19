e868721880aa69aa1d72701ebe5f0b4767175320
Rafael L <118671591+rafaellopesss@users.noreply.github.com>
2024-01-18 23:10:02 -0300

    atualizar

--- aula1/aula1.py
+++ aula1/aula1.py
@@ -1,5 +1,14 @@
-numero = int(input("Digite um número:"))
-numero_2 = int(input("Digite mais um:"))
+import pyautogui #Biblioteca automatizada
+from time import sleep
+import webbrowser
 
-print(f"O resultado é {numero + numero_2}")
-
+pyautogui.alert('O código vai começar')
+webbrowser.open("https://web.whatsapp.com/")
+sleep(10)
+pyautogui.click(816,444, duration=5)
+sleep(5)
+pyautogui.click(1180,645, duration=3)
+sleep(2)
+pyautogui.write('Bora casar logo')
+sleep(2)
+pyautogui.press('enter')
