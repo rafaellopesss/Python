b07cb5e9fc8dbd0fb10a5c3dfc10bdd6dcc4175c
Rafael L. <118671591+rafaellopesss@users.noreply.github.com>
2024-11-26 10:42:36 -0300

    att

--- whatsaap/document.py
+++ whatsaap/document.py
@@ -1,6 +1,3 @@
 import pywhatkit
 
-contatos = ['+558598537885', '+558695567575', '+558699362080']
-
-for item in contatos:
-    pywhatkit.sendwhatmsg_instantly(item, 'Oi')
+pywhatkit.sendwhatmsg_instantly("+558699362080", 'oi')
